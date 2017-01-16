015146f73e42728928b8cf22e6535a0ab28f40a3
Chyld Medford <chyld.medford@gmail.com>
2017-01-16 14:09:10 +0530

    wip

--- src/test/java/com/allstate/services/MovieServiceTest.java
+++ src/test/java/com/allstate/services/MovieServiceTest.java
@@ -7,12 +7,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Sql(value = {"/sql/seed.sql"})
 public class MovieServiceTest {
     @Autowired
     private MovieService service;
@@ -32,6 +34,15 @@ public class MovieServiceTest {
         Movie before = new Movie();
         before.setTitle("The Matrix");
         Movie after = this.service.create(before);
-        assertTrue(after.getId() > 0);
+        assertEquals(2, after.getId());
+        assertEquals(0, after.getVersion());
+        assertEquals("The Matrix", after.getTitle());
+    }
+
+    @Test(expected = org.springframework.dao.DataIntegrityViolationException.class)
+    public void shouldNotCreateMovieNoTitle() throws Exception {
+        Movie before = new Movie();
+        Movie after = this.service.create(before);
+        assertEquals(2, after.getId());
     }
 }
---
+++ src/test/resources/sql/seed.sql
@@ -0,0 +1,6 @@
+use bollywoodtst;
+
+TRUNCATE TABLE movies;
+
+INSERT INTO movies (title, rating, released, length, watched) VALUES
+  ('The Avengers', 'PG', '2015-03-15 00:00:00.000', 165, 1);
